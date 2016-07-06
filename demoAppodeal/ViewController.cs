1f6116722d491122381246575879f64360d02f0a
denplenkin <denplenkin@gmail.com>
2016-07-06 19:47:11 +0300

    Update ViewController.cs

--- demoAppodeal/ViewController.cs
+++ demoAppodeal/ViewController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Foundation;
 using UIKit;
 using AppodealBinding;
@@ -28,32 +28,48 @@ namespace demoAppodeal
 		{
 		}
 
-		AppodealNativeAdViewAttributes attributes;
-		AppodealNativeAdView adView;
+		//AppodealNativeAdViewAttributes attributes;
+		//AppodealNativeAdView adView;
 		AppodealNativeAdService adService;
 		AppodealNativeAd ad;
 
 		public void MyDelegateServiceDidLoad(AppodealNativeAd nativeAd)
 		{
+//			this.ad = new AppodealNativeAd ();
+//			this.ad = nativeAd;
+//
+//			this.attributes = new AppodealNativeAdViewAttributes();
+//			this.attributes.Width=320;
+//			this.attributes.Heigth=50;
+//
+//			this.adView = AppodealNativeAdView.NativeAdViewWithType(AppodealNativeAdViewType.ContentStream, this.ad, this.attributes, UIApplication.SharedApplication.Windows[0].RootViewController);
+//			UIView view = new UIView ();
+//			view.Frame = new System.Drawing.Rectangle(10, 400, 320, 50);
+//			view.AddSubview(this.adView);
+//			View.AddSubview(view);
+
 			this.ad = new AppodealNativeAd ();
 			this.ad = nativeAd;
 
-			this.attributes = new AppodealNativeAdViewAttributes();
-			this.attributes.Width=320;
-			this.attributes.Heigth=50;
-
-			this.adView = AppodealNativeAdView.NativeAdViewWithType(AppodealNativeAdViewType.ContentStream, this.ad, this.attributes, UIApplication.SharedApplication.Windows[0].RootViewController);
 			UIView view = new UIView ();
-			view.Frame = new System.Drawing.Rectangle(10, 400, 320, 50);
-			view.AddSubview(this.adView);
+			view.Frame = new System.Drawing.Rectangle(10, 350, 200, 200);
+
+			var data = NSData.FromUrl(this.ad.Image.ImageUrl);
+	
+			UIImageView imageview = new UIImageView (UIImage.LoadFromData (data));
+			imageview.Frame = new System.Drawing.Rectangle(0 ,0, 50,50);
+
+			view.AddSubview(imageview);
+
 			View.AddSubview(view);
+			this.ad.AttachToView(view, UIApplication.SharedApplication.Windows[0].RootViewController);
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
-
+			Appodeal.DisableNetworkForAdType(AppodealAdType.All, NetworkNames.kAppodealFacebookNetworkName);
 			Appodeal.InitializeWithApiKey("dee74c5129f53fc629a44a690a02296694e3eef99f2d3a5f", AppodealAdType.All);
 
 			BannerButton.TouchUpInside += (object sender, EventArgs e) => {
